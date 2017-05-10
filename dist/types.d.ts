d2a94eacd6f0668131adc7260204881e31ea71c3
Benny Neugebauer <bn@bennyn.de>
2017-05-10 11:13:54 +0200

    dist/types.d.ts

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,10 +1,13 @@
-/** @namespace */
-export declare module UUID {
+/** @class */
+declare class UUID {
+   /** @class */
+   constructor();
+
    /**
     * Generates a version 4 UUID as a hexadecimal string.
     * @returns {string} Hexadecimal UUID string.
     */
-   function generate(): string;
+   static generate(): string;
 
    /**
     * Returns an unsigned x-bit random integer.
@@ -12,7 +15,7 @@ export declare module UUID {
     * @param {number} x Positive integer ranging from 0 to 53, inclusive.
     * @returns {number} Unsigned x-bit random integer (0 <= f(x) < 2^x).
     */
-   function _getRandomInt(x: number): number;
+   private static _getRandomInt(x: number): number;
 
    /**
     * Converts an integer to a zero-filled hexadecimal string.
@@ -21,13 +24,13 @@ export declare module UUID {
     * @param {number} length
     * @returns {string}
     */
-   function _hexAligner(num: number, length: number): string;
+   private static _hexAligner(num: number, length: number): string;
 
    /**
     * Retains the value of 'UUID' global variable assigned before loading UUID.js.
     * @since 3.2
     */
-   var overwrittenUUID: any;
+   static overwrittenUUID: any;
 
 }
 
