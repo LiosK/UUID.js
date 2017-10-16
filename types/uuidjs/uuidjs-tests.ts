3de36af31d103a4b9d54d8eb23a2f2c4ee1344a0
LiosK <contact@mail.liosk.net>
2017-10-16 01:01:36 -0400

    Refine TypeScript declaration tests

--- types/uuidjs/uuidjs-tests.ts
+++ types/uuidjs/uuidjs-tests.ts
@@ -1,10 +1,13 @@
 import * as UUID from 'uuidjs';
 
-let strV4: string = UUID.generate();
+const putNum = (x: number) => { console.log(x + ': number') };
+const putStr = (x: string) => { console.log('"' + x + '": string') };
+const putBool = (x: boolean) => { console.log(x + ': boolean') };
 
+putStr(UUID.generate());
 let objV4: UUID.UUID = UUID.genV4();
 let objV1: UUID.UUID = UUID.genV1();
-let parsed: UUID.UUID = UUID.parse(strV4);
+let parsed: UUID.UUID = UUID.parse(objV4.toString());
 
 let _FIELD_NAMES: Array<string> = UUID.FIELD_NAMES;
 let _FIELD_SIZES: Array<number> = UUID.FIELD_SIZES;
@@ -14,40 +17,38 @@ let _intFields: UUID.UUIDFields<number> = objV4.intFields;
 let _bitFields: UUID.UUIDFields<string> = objV4.bitFields;
 let _hexFields: UUID.UUIDFields<string> = objV4.hexFields;
 
-let num: number;
-let str: string;
 for (let i: number = 0; i < 6; i++) {
-  num = _intFields[i];
-  str = _bitFields[i];
-  str = _hexFields[i];
+  putNum(_intFields[i]);
+  putStr(_bitFields[i]);
+  putStr(_hexFields[i]);
 }
 
-num = _intFields.timeLow;
-num = _intFields.timeMid;
-num = _intFields.timeHiAndVersion;
-num = _intFields.clockSeqHiAndReserved;
-num = _intFields.clockSeqLow;
-num = _intFields.node;
-str = _bitFields.timeLow;
-str = _bitFields.timeMid;
-str = _bitFields.timeHiAndVersion;
-str = _bitFields.clockSeqHiAndReserved;
-str = _bitFields.clockSeqLow;
-str = _bitFields.node;
-str = _hexFields.timeLow;
-str = _hexFields.timeMid;
-str = _hexFields.timeHiAndVersion;
-str = _hexFields.clockSeqHiAndReserved;
-str = _hexFields.clockSeqLow;
-str = _hexFields.node;
+putNum(_intFields.timeLow);
+putNum(_intFields.timeMid);
+putNum(_intFields.timeHiAndVersion);
+putNum(_intFields.clockSeqHiAndReserved);
+putNum(_intFields.clockSeqLow);
+putNum(_intFields.node);
+putStr(_bitFields.timeLow);
+putStr(_bitFields.timeMid);
+putStr(_bitFields.timeHiAndVersion);
+putStr(_bitFields.clockSeqHiAndReserved);
+putStr(_bitFields.clockSeqLow);
+putStr(_bitFields.node);
+putStr(_hexFields.timeLow);
+putStr(_hexFields.timeMid);
+putStr(_hexFields.timeHiAndVersion);
+putStr(_hexFields.clockSeqHiAndReserved);
+putStr(_hexFields.clockSeqLow);
+putStr(_hexFields.node);
 
-let _version: number = objV4.version;
-let _bitString: string = objV4.bitString;
-let _hexNoDelim: string = objV4.hexNoDelim;
-let _hexString: string = objV4.hexString;
-let _urn: string = objV4.urn;
-let _toString: string = objV4.toString();
-let _equals: boolean = objV4.equals(objV1);
+putNum(objV4.version);
+putStr(objV4.bitString);
+putStr(objV4.hexNoDelim);
+putStr(objV4.hexString);
+putStr(objV4.urn);
+putStr(objV4.toString());
+putBool(objV4.equals(objV1));
 
 UUID.resetState();
 UUID.useMathRandom();
